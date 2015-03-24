45842a2d2e9203b3f8e40b3e92de37bd71674cba
Ivan Topolnjak <ivantopo@gmail.com>
2015-03-24 23:55:05 +0100

    start integrating the new theme in the documentation pages.

--- assets/js/kamon.js
+++ assets/js/kamon.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
 	$('label.tree-toggler').click(function () {
 		$(this).parent().children('ul.tree').toggle(300);
 	});
+
+	// tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+	$(function(){
+	  $('a').each(function() {
+	    if ($(this).prop('href') == window.location.href) {
+	      $(this).addClass('active');
+	    }
+	  });
+	});
+
 });
