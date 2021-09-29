844b08e05729b0de6ac11a06cdc8cb33a3af4a75
Luka Skukan <lskukan1@gmail.com>
2021-09-29 16:03:06 +0200

    (feat) New Blog List
    * Implement Load More behaviour
    * Adjust heading and such to match the landing page(s)
    * Tweak styles to match the design

---
+++ assets/js/blogs.js
@@ -0,0 +1,30 @@
+const perPage = 6
+let currentPage = 1
+
+function adjustVisibility() {
+  const posts = $('.js-blog-card')
+  const currentlyVisible = currentPage * perPage
+
+  posts.each((i, el) => {
+    const $e = $(el)
+    if (i < currentlyVisible) {
+      $e.removeClass('invisible')
+    }
+  })
+
+  const loadMoreButton = $('.js-load-more')
+
+  if (currentlyVisible > posts.length) {
+    loadMoreButton.addClass('invisible')
+  }
+}
+
+function loadMore() {
+  currentPage++
+  adjustVisibility()
+}
+
+$(document).ready(() => {
+  adjustVisibility()
+  $('.js-load-more').on('click', loadMore)
+})
